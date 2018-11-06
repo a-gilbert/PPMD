cf798aebc59750cb4e8db247961c89163fa73181
Austin <agilbert39@gatech.edu>
2018-11-06 00:01:42 -0800

    Params finished, but need to be tested.

--- src/params.py
+++ src/params.py
@@ -1,10 +1,9 @@
 """Various enums and dicts to define the meta-state of a simulation."""
-
-from enum import Enum 
+from enum import Enum
+from mpi4py import MPI
 
 
 class RefineCriteria(Enum):
-	NONE = 0
 	MAX_DEPTH = 1
 	MAX_DENS = 2
 	MAX_LENGTH = 3
@@ -15,11 +14,151 @@ class ForceType(Enum):
 	FMM = 1
 
 
+class Units(Enum):
+	DIMENSIONLESS = 0
+	CGS = 1
+	SI = 2
+
+
+class PartitionScheme(Enum):
+	GRID = 0
+	ORB = 1
+
+
+class SpatialBoundaries(Enum):
+	FIXED = 0
+	PERIODIC = 1
+
+
+class ForceBoundaries(Enum):
+	FIXED = 0
+	PERIODIC = 1
+
+
+class ParticleIC(Enum):
+	RANDOM = 0
+	CUSTOM = 1
+
+
+class GPK(Enum):
+	UNITS = 0
+	DIMS = 1
+	BBOX = 2
+	SBTYPE = 3
+	FBTYPE = 4
+	FTYPE = 5
+	PSCHEME = 6
+	PIC = 7
+	RCRIT = 8
+	NSTEPS = 9
+	CSTEP = 10
+	OUTFREQ = 11
+	OUTFN = 12
+	DT = 13
+	NPART = 14
+	GPUS = 15
+
+
 class GlobalParams(dict):
+	def __init__(self, infile, comm, rank):
+		super(GlobalParams, self).__init__()
+		self._empty_init()
+		if rank == 0:
+			in_file = open(user_in, 'r')
+			content = in_file.readlines()
+			in_file.close()
+			for line in content:
+			    self.interpret(line)
+			self.check_params_set()
+		self.set_global(comm, rank)
 	
+	def _empty_init(self):
+		for k in list(GPK):
+			self[k] = None
 
 
+	def check_params_set(self):
+		for k in list(GPK):
+			if self[k] == None:
+				raise Exception("Parameter not set!", k)
 
 
+	def interpret(self, line):
+		l = line.split()
+		if l[0] == "#":
+			pass
+		elif l[0] == "Units":
+			if l[2] == "none":
+				self[GPK.UNITS] = Units.DIMENSIONLESS
+			elif l[2] == 'si':
+				self[GPK.UNITS] = Units.SI
+			elif l[2] == 'cgs':
+				self[GPK.UNITS] = Units.CGS
+		elif l[0] == "Dimensions":
+			self[GPK.DIMS] = int(l[2])
+		elif l[0] == "Box":
+			self[GPK.BBOX] = []
+			if self[GPK.DIMS] == 3:
+				self[GPK.BBOX].append([float(l[2]), float(l[3]), float(l[4])])
+				self[GPK.BBOX].append([float(l[5]), float(l[6]), float(l[7])])
+			elif self[GPK.DIMS] == 2:
+				self[GPK.BBOX].append([float(l[2]), float(l[3])])
+				self[GPK.BBOX].append([float(l[4]), float(l[5])])
+			elif self[GPK.DIMS] == 1:
+				self[GPK.BBOX].append([float(l[2])])
+				self[GPK.BBOX].append([float(l[3])]) 
+		elif l[0] == "SpaceBC":
+			if l[2] == "Fixed":
+				self[GPK.SBTYPE] = SpatialBoundaries.FIXED
+			if l[2] == "Periodic":
+				self[GPK.SBTYPE] = SpatialBoundaries.PERIODIC
+		elif l[0] == "ForceBC":
+			if l[2] == "Fixed":
+				self[GPK.FBTYPE] = ForceBoundaries.FIXED
+			if l[2] == "Periodic":
+				self[GPK.FBTYPE] = ForceBoundaries.PERIODIC
+		elif l[0] == "ForceType":
+			if l[2] == "Exact":
+				self[GPK.FTYPE] = ForceType.EXACT
+			if l[2] == "FMM":
+				self[GPK.FTYPE] = ForceType.FMM
+		elif l[0] == "PartitionType":
+			if l[2] == "Grid":
+				self[GPK.PSCHEME] = PartitionScheme.GRID
+			if l[2] == "ORB":
+				self[GPK.PSCHEME] = PartitionScheme.ORB
+		elif l[0] == "ParticleIC":
+			if l[2] == "Random":
+				self[GPK.PIC] = ParticleIC.RANDOM
+			else:
+				self[GPK.PIC] = [ParticleIC.CUSTOM, l[2]]
+		elif l[0] == "Refinement":
+			self[GPK.RCRIT] = []
+			for i in range(2, len(l), 2):
+				if l[i] == "Depth":
+					self[GPK.RCRIT].append([RefineCriteria.MAX_DEPTH, int(l[i+1])])
+				elif l[i] == "Density":
+					self[GPK.RCRIT].append([RefineCriteria.MAX_DENS, float(l[i+1])])
+				elif l[i] == "Length":
+					self[GPK.RCRIT].append([RefineCriteria.MAX_LENGTH, float(l[i+1])])
+		elif l[0] == "NSteps":
+			self[GPK.NSTEPS] = int(l[2])
+			self[GPK.CSTEP] = 0
+		elif l[0] == "OutFreq":
+			self[GPK.OUTFREQ] = int(l[2])
+		elif l[0] == "OutFn":
+			self[GPK.OUTFN] = l[2]
+		elif l[0] == "dt":
+			self[GPK.DT] = float(l[2])
+		elif l[0] == "GPUS":
+			if l[2] == "True":
+				self[GPK.GPUS] = True
+			elif l[2] == "False":
+				self[GPK.GPUS] = False
+		elif l[0] == "NParticles":
+			self[GPK.NPART] = int(l[2])
 
 
+	def set_global(self, comm, rank):
+		for k in list(GPK):
+			self[k] = comm.bcast(self[k], root=0)
---
+++ tests/params_test/params_test.py
@@ -0,0 +1,8 @@
+import sys
+
+sys.path.append("../../")
+
+from src.params import GlobalParams
+from mpi4py import MPI
+
+
