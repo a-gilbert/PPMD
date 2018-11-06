72c5d8e19c69786bbda9e2029f20f646b4a98005
Austin <agilbert39@gatech.edu>
2018-11-06 08:41:19 -0800

    unit testing on global params partially completed.

--- src/params.py
+++ src/params.py
@@ -1,6 +1,5 @@
 """Various enums and dicts to define the meta-state of a simulation."""
 from enum import Enum
-from mpi4py import MPI
 
 
 class RefineCriteria(Enum):
@@ -60,11 +59,11 @@ class GPK(Enum):
 
 
 class GlobalParams(dict):
-	def __init__(self, infile, comm, rank):
+	def __init__(self, in_file, comm, rank):
 		super(GlobalParams, self).__init__()
 		self._empty_init()
 		if rank == 0:
-			in_file = open(user_in, 'r')
+			in_file = open(in_file, 'r')
 			content = in_file.readlines()
 			in_file.close()
 			for line in content:
@@ -85,78 +84,84 @@ class GlobalParams(dict):
 
 	def interpret(self, line):
 		l = line.split()
-		if l[0] == "#":
-			pass
-		elif l[0] == "Units":
-			if l[2] == "none":
-				self[GPK.UNITS] = Units.DIMENSIONLESS
-			elif l[2] == 'si':
-				self[GPK.UNITS] = Units.SI
-			elif l[2] == 'cgs':
-				self[GPK.UNITS] = Units.CGS
-		elif l[0] == "Dimensions":
-			self[GPK.DIMS] = int(l[2])
-		elif l[0] == "Box":
-			self[GPK.BBOX] = []
-			if self[GPK.DIMS] == 3:
-				self[GPK.BBOX].append([float(l[2]), float(l[3]), float(l[4])])
-				self[GPK.BBOX].append([float(l[5]), float(l[6]), float(l[7])])
-			elif self[GPK.DIMS] == 2:
-				self[GPK.BBOX].append([float(l[2]), float(l[3])])
-				self[GPK.BBOX].append([float(l[4]), float(l[5])])
-			elif self[GPK.DIMS] == 1:
-				self[GPK.BBOX].append([float(l[2])])
-				self[GPK.BBOX].append([float(l[3])]) 
-		elif l[0] == "SpaceBC":
-			if l[2] == "Fixed":
-				self[GPK.SBTYPE] = SpatialBoundaries.FIXED
-			if l[2] == "Periodic":
-				self[GPK.SBTYPE] = SpatialBoundaries.PERIODIC
-		elif l[0] == "ForceBC":
-			if l[2] == "Fixed":
-				self[GPK.FBTYPE] = ForceBoundaries.FIXED
-			if l[2] == "Periodic":
-				self[GPK.FBTYPE] = ForceBoundaries.PERIODIC
-		elif l[0] == "ForceType":
-			if l[2] == "Exact":
-				self[GPK.FTYPE] = ForceType.EXACT
-			if l[2] == "FMM":
-				self[GPK.FTYPE] = ForceType.FMM
-		elif l[0] == "PartitionType":
-			if l[2] == "Grid":
-				self[GPK.PSCHEME] = PartitionScheme.GRID
-			if l[2] == "ORB":
-				self[GPK.PSCHEME] = PartitionScheme.ORB
-		elif l[0] == "ParticleIC":
-			if l[2] == "Random":
-				self[GPK.PIC] = ParticleIC.RANDOM
+		if len(l) > 0:
+			if l[0] == "#":
+				pass
+			elif l[0] == "Units":
+				if l[2] == "none":
+					self[GPK.UNITS] = Units.DIMENSIONLESS
+				elif l[2] == 'si':
+					self[GPK.UNITS] = Units.SI
+				elif l[2] == 'cgs':
+					self[GPK.UNITS] = Units.CGS
+			elif l[0] == "Dimensions":
+				self[GPK.DIMS] = int(l[2])
+			elif l[0] == "Box":
+				self[GPK.BBOX] = []
+				if self[GPK.DIMS] == 3:
+					self[GPK.BBOX].append([float(l[2]), float(l[3]), float(l[4])])
+					self[GPK.BBOX].append([float(l[5]), float(l[6]), float(l[7])])
+				elif self[GPK.DIMS] == 2:
+					self[GPK.BBOX].append([float(l[2]), float(l[3])])
+					self[GPK.BBOX].append([float(l[4]), float(l[5])])
+				elif self[GPK.DIMS] == 1:
+					self[GPK.BBOX].append([float(l[2])])
+					self[GPK.BBOX].append([float(l[3])]) 
+			elif l[0] == "SpaceBC":
+				if l[2] == "Fixed":
+					self[GPK.SBTYPE] = SpatialBoundaries.FIXED
+				if l[2] == "Periodic":
+					self[GPK.SBTYPE] = SpatialBoundaries.PERIODIC
+			elif l[0] == "ForceBC":
+				if l[2] == "Fixed":
+					self[GPK.FBTYPE] = ForceBoundaries.FIXED
+				if l[2] == "Periodic":
+					self[GPK.FBTYPE] = ForceBoundaries.PERIODIC
+			elif l[0] == "ForceType":
+				if l[2] == "Exact":
+					self[GPK.FTYPE] = ForceType.EXACT
+				if l[2] == "FMM":
+					self[GPK.FTYPE] = ForceType.FMM
+			elif l[0] == "PartitionType":
+				if l[2] == "Grid":
+					self[GPK.PSCHEME] = PartitionScheme.GRID
+				if l[2] == "ORB":
+					self[GPK.PSCHEME] = PartitionScheme.ORB
+			elif l[0] == "ParticleIC":
+				if l[2] == "Random":
+					self[GPK.PIC] = ParticleIC.RANDOM
+				else:
+					self[GPK.PIC] = [ParticleIC.CUSTOM, l[2]]
+			elif l[0] == "Refinement":
+				self[GPK.RCRIT] = []
+				for i in range(2, len(l), 2):
+					if l[i] == "Depth":
+						self[GPK.RCRIT].append([RefineCriteria.MAX_DEPTH,
+						 int(l[i+1])])
+					elif l[i] == "Density":
+						self[GPK.RCRIT].append([RefineCriteria.MAX_DENS,
+						 float(l[i+1])])
+					elif l[i] == "Length":
+						self[GPK.RCRIT].append([RefineCriteria.MAX_LENGTH,
+						 float(l[i+1])])
+			elif l[0] == "NSteps":
+				self[GPK.NSTEPS] = int(l[2])
+				self[GPK.CSTEP] = 0
+			elif l[0] == "OutFreq":
+				self[GPK.OUTFREQ] = int(l[2])
+			elif l[0] == "OutFn":
+				self[GPK.OUTFN] = l[2]
+			elif l[0] == "dt":
+				self[GPK.DT] = float(l[2])
+			elif l[0] == "GPUS":
+				if l[2] == "True":
+					self[GPK.GPUS] = True
+				elif l[2] == "False":
+					self[GPK.GPUS] = False
+			elif l[0] == "NParticles":
+				self[GPK.NPART] = int(l[2])
 			else:
-				self[GPK.PIC] = [ParticleIC.CUSTOM, l[2]]
-		elif l[0] == "Refinement":
-			self[GPK.RCRIT] = []
-			for i in range(2, len(l), 2):
-				if l[i] == "Depth":
-					self[GPK.RCRIT].append([RefineCriteria.MAX_DEPTH, int(l[i+1])])
-				elif l[i] == "Density":
-					self[GPK.RCRIT].append([RefineCriteria.MAX_DENS, float(l[i+1])])
-				elif l[i] == "Length":
-					self[GPK.RCRIT].append([RefineCriteria.MAX_LENGTH, float(l[i+1])])
-		elif l[0] == "NSteps":
-			self[GPK.NSTEPS] = int(l[2])
-			self[GPK.CSTEP] = 0
-		elif l[0] == "OutFreq":
-			self[GPK.OUTFREQ] = int(l[2])
-		elif l[0] == "OutFn":
-			self[GPK.OUTFN] = l[2]
-		elif l[0] == "dt":
-			self[GPK.DT] = float(l[2])
-		elif l[0] == "GPUS":
-			if l[2] == "True":
-				self[GPK.GPUS] = True
-			elif l[2] == "False":
-				self[GPK.GPUS] = False
-		elif l[0] == "NParticles":
-			self[GPK.NPART] = int(l[2])
+				pass
 
 
 	def set_global(self, comm, rank):
--- tests/params_test/params_test.py
+++ tests/params_test/params_test.py
@@ -2,7 +2,19 @@ import sys
 
 sys.path.append("../../")
 
-from src.params import GlobalParams
+from src.params import GlobalParams, GPK, ParticleIC, ForceBoundaries, \
+SpatialBoundaries, PartitionScheme, Units, ForceType, RefineCriteria
 from mpi4py import MPI
 
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
 
+params = GlobalParams("test_sim.ppmd", comm, rank)
+
+tests = []
+
+tests.append(params[GPK.UNITS] == Units.SI)
+tests.append(params[GPK.DIMS] == 3)
+tests.append(params[GPK.BBOX] == [[0.0, 0.0, 0.0], [1.0, 1.0, 1.0]])
+tests.append(params[GPK.SBTYPE] == SpatialBoundaries.PERIODIC)
+print(tests)
